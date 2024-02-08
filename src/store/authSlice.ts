ec2333e480378d7be8257640441a58e53d582949
Dima <dimadeg2202@gmail.com>
2024-02-08 22:19:48 +0500

    Added redux and auth state

---
+++ src/store/authSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { AUTH } from 'constants/app.constants'
+import Cookies from 'js-cookie'
+
+type State = {
+  isAuth: boolean
+}
+
+const initialValue = Cookies.get(AUTH) || false
+
+const initialState:State = {
+	isAuth: initialValue
+}
+
+const authSlice = createSlice({
+	name: 'auth',
+	initialState,
+	reducers: {
+		login(state) {
+			state.isAuth = true
+			Cookies.set(AUTH, true)
+		},
+
+		logout(state) {
+			state.isAuth = false
+			Cookies.remove(AUTH)
+		}
+	}
+})
+
+export const { login, logout } = authSlice.actions
+
+export default authSlice.reducer
