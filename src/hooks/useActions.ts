867b7e475f9fac9e052d9a7ac4091adbf7739c63
Dima <dimadeg2202@gmail.com>
2024-02-08 22:22:01 +0500

    Hook for redux for select and dispatch

---
+++ src/hooks/useActions.ts
@@ -0,0 +1,21 @@
+import { bindActionCreators } from '@reduxjs/toolkit'
+import { useMemo } from 'react'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import * as authActions from 'store/authSlice'
+import { AppDispatch, RootState } from 'store/store'
+// import * as themeActions from 'store/themeSlice'
+
+const rootActions = {
+	...authActions,
+	// ...themeActions
+}
+
+export const useActions = () => {
+	const dispatch = useDispatch<AppDispatch>()
+
+	return useMemo(() => {
+		return bindActionCreators(rootActions, dispatch)
+	}, [dispatch])
+}
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
