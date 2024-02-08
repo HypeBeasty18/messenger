999deb77ced35e8f383c728ab2544d39ccb44601
Dima <dimadeg2202@gmail.com>
2024-02-08 22:23:35 +0500

    Types for env

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,13 @@
-
-
 export type TDataInput = {
-  username: string,
-  password: string
+	username: string
+	password: string
 }
+
+export type TMessage = {
+	name: string
+	lastMessage: string
+	img: string
+	isOnline: boolean
+	isPinned: boolean
+}
+
