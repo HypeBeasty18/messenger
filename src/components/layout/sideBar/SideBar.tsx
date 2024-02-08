ea7282731573fbb556b16171af2c1cc66d789656
Dima <dimadeg2202@gmail.com>
2024-02-08 22:31:58 +0500

    Element contains all important info

--- src/components/layout/sideBar/SideBar.tsx
+++ src/components/layout/sideBar/SideBar.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { FC, useState } from 'react'
+import { PiNotePencil } from 'react-icons/pi'
 
-type Props = {}
+import s from './SideBar.module.scss'
+import AllMessages from './allMessages/AllMessages'
+import { messages } from './messages.list'
+import PinnedMessage from './pinnedMesssage/PinnedMessage'
+import SearchInput from './ui/searchInput/SearchInput'
 
-const SideBar = (props: Props) => {
-  return (
-    <div>SideBar</div>
-  )
+const SideBar: FC = () => {
+	const [isFinded, setIsFinded] = useState<boolean>(false)
+
+	return (
+		<div className={s.container}>
+			<div className={s.upInner}>
+				<div className={s.header}>
+					<h2>
+						Messages <span>({messages.length})</span>
+					</h2>
+					<button>
+						<PiNotePencil className={s.createIcon} />
+					</button>
+				</div>
+				<SearchInput setIsFinded={setIsFinded} />
+			</div>
+			{!isFinded && (
+				<>
+					<PinnedMessage />
+					<AllMessages />	
+				</>
+			)}
+		</div>
+	)
 }
 
 export default SideBar
