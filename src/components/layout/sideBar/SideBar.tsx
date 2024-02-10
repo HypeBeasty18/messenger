d535b61b1233dad9e81d21d8236d8fccfae6801c
Dima <dimadeg2202@gmail.com>
2024-02-10 22:00:55 +0500

    Added useEffect for save messages from server

--- src/components/layout/sideBar/SideBar.tsx
+++ src/components/layout/sideBar/SideBar.tsx
@@ -1,21 +1,44 @@
-import { FC, useState } from 'react'
+import { doc, onSnapshot } from 'firebase/firestore'
+import { auth, db } from 'firebaseConfig/firebase'
+import { useActions, useAppSelector } from 'hooks/useActions'
+import { FC, useEffect, useState } from 'react'
 import { PiNotePencil } from 'react-icons/pi'
 
 import s from './SideBar.module.scss'
 import AllMessages from './allMessages/AllMessages'
-import { messages } from './messages.list'
 import PinnedMessage from './pinnedMesssage/PinnedMessage'
 import SearchInput from './ui/searchInput/SearchInput'
 
 const SideBar: FC = () => {
 	const [isFinded, setIsFinded] = useState<boolean>(false)
 
+	const actions = useActions()
+	const messages = useAppSelector(state => state.messages)
+
+	useEffect(() => {
+		const getChats = () => {
+			if (auth.currentUser) {
+				return onSnapshot(doc(db, 'userChats', auth.currentUser.uid), doc => {
+					const data = doc.data()
+					if (data) {
+						actions.saveMessages(data)
+					}
+				})
+			}
+		}
+		const unsubscribe = getChats()
+		return () => {
+			unsubscribe
+		}
+	}, [auth.currentUser])
+
 	return (
 		<div className={s.container}>
 			<div className={s.upInner}>
 				<div className={s.header}>
 					<h2>
-						Messages <span>({messages.length})</span>
+						Messages{' '}
+						<span>({messages ? Object.entries(messages).length : 0})</span>
 					</h2>
 					<button>
 						<PiNotePencil className={s.createIcon} />
@@ -26,7 +49,7 @@ const SideBar: FC = () => {
 			{!isFinded && (
 				<>
 					<PinnedMessage />
-					<AllMessages />	
+					<AllMessages />
 				</>
 			)}
 		</div>
