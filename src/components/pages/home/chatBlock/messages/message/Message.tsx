a7b3118bd212490f64695464436acde779ddea93
Dima <dimadeg2202@gmail.com>
2024-02-10 21:38:42 +0500

    Ui component for message

---
+++ src/components/pages/home/chatBlock/messages/message/Message.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import s from './Message.module.scss'
+type Props = {}
+
+const Message = (props: Props) => {
+  return (
+    <div>Message</div>
+  )
+}
+
+export default Message
