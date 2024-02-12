5aae70b14b302b4065856f081d1082b7c3698e58
Dima <dimadeg2202@gmail.com>
2024-02-12 12:26:42 +0500

    Added start page for chat block

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,15 +1,28 @@
 import Layout from 'components/layout/Layout'
+import { useAppSelector } from 'hooks/useActions'
 
 import s from './Home.module.scss'
 import ChatBlock from './chatBlock/ChatBlock'
 import UpChatBlock from './upChatBlock/UpChatBlock'
+import { FC } from 'react'
 
-const Home = () => {
+const Home:FC = () => {
+	const chatId = useAppSelector(state => state.currentChat.chatId)
+	
+
 	return (
 		<Layout>
 			<div className={s.container}>
-				<UpChatBlock />
-				<ChatBlock />
+				{chatId ? (
+					<>
+						<UpChatBlock />
+						<ChatBlock />
+					</>
+				) : (
+					<div className={s.emptyChat}>
+						<span>Select a chat to start messaging</span>
+					</div>
+				)}
 			</div>
 		</Layout>
 	)
