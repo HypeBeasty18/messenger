b8210d732984c7764d8358d37d0adf4a9a07ade1
Dima <dimadeg2202@gmail.com>
2024-02-12 12:25:04 +0500

    Added uses 2 ui component for different users

--- src/components/pages/home/chatBlock/messages/Messages.tsx
+++ src/components/pages/home/chatBlock/messages/Messages.tsx
@@ -1,32 +1,78 @@
 import { doc, onSnapshot } from 'firebase/firestore'
-import { db } from 'firebaseConfig/firebase'
+import { auth, db } from 'firebaseConfig/firebase'
 import { useAppSelector } from 'hooks/useActions'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import s from './Messages.module.scss'
+import MessageMe from './messageMe/MessageMe'
+import MessageYou from './messageYou/MessageYou'
 
-type Props = {
-  messages: []
+type TMessage = {
+	m: {
+		date: {
+			seconds: number
+			nanoseconds: number
+		}
+		id: string
+		senderId: string
+		text: string
+	}
 }
 
 const Messages = () => {
-	const [messages, setMessages] = useState<Props | null>(null)
-
+	const [messages, setMessages] = useState<[]>([])
+	const [prevSenderId, setPrevSenderId] = useState<string>('')
 	const chatId = useAppSelector(state => state.currentChat.chatId)
+
+	const currentRef = useRef('')
 
 	useEffect(() => {
 		if (chatId) {
 			const unsub = onSnapshot(doc(db, 'chats', chatId), doc => {
-				doc.exists() && doc.data && setMessages(doc.data)
-				console.log(messages)
+				doc.exists() && setMessages(doc.data().messages)
 			})
-      return () => {
-        unsub()
-      }
+			return () => {
+				unsub()
+			}
 		}
 	}, [chatId])
 
-	return <div className={s.container}></div>
+	return (
+		<div className={s.container}>
+			<div className={s.startDialog}>
+				<p>Dialog beginning</p>
+			</div>
+			{messages.length > 0 &&
+				messages.map((m, index) => {
+					let isNextSameRender = false
+					currentRef.current = m.senderId
+					if (
+						index < messages.length - 1 &&
+						currentRef.current === messages[index + 1].senderId
+					) {
+						isNextSameRender = true
+					}
+
+					return (
+						<>
+							{auth.currentUser && auth.currentUser.uid === m.senderId ? (
+								<MessageMe
+									message={m}
+									key={index}
+									isNextSameRender={isNextSameRender}
+								/>
+							) : (
+								<MessageYou
+									message={m}
+									key={index}
+									isNextSameRender={isNextSameRender}
+								/>
+							)}
+						</>
+					)
+				})}
+		</div>
+	)
 }
 
 export default Messages
