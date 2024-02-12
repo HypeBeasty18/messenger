0d406b8e51a5269afe77d0ed320995fcabf2a9a1
Dima <dimadeg2202@gmail.com>
2024-02-12 23:20:52 +0500

    Convert timestamp format data in date

--- src/store/messagesSlice.ts
+++ src/store/messagesSlice.ts
@@ -1,7 +1,8 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { Timestamp } from 'firebase/firestore'
 import { Chat } from 'types/types'
 
-interface State {
+export interface State {
 	[key: string]: Chat
 }
 
@@ -11,10 +12,17 @@ const authSlice = createSlice({
 	name: 'messages',
 	initialState,
 	reducers: {
-		saveMessages(state, action: PayloadAction<{ [key: string]: Chat }>) {
-			return action.payload
-		},
-		removeMessages(state){
+    saveMessages(state, action: PayloadAction<{ [key: string]: Chat }>) {
+      Object.keys(action.payload).forEach(key => {
+        const chat = action.payload[key]
+        if (chat.date instanceof Timestamp) {
+          const timestamp = chat.date;
+          chat.date = timestamp.seconds * 1000 + Math.floor(timestamp.nanoseconds / 1000000);					
+				}
+      })
+      return action.payload;
+    },
+		removeMessages(){
 			return {}
 		}
 	}
