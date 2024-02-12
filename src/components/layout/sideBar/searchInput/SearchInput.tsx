b8c73db6869f29123cef270aff735d0de1838b89
Dima <dimadeg2202@gmail.com>
2024-02-12 23:26:39 +0500

    Input for search other users

---
+++ src/components/layout/sideBar/searchInput/SearchInput.tsx
@@ -0,0 +1,56 @@
+import { auth } from 'firebaseConfig/firebase'
+import { useOnClickOutside } from 'hooks/useOnClickOutside'
+import React, { FC, useEffect, useState } from 'react'
+import { CiSearch } from 'react-icons/ci'
+
+import s from './SearchInput.module.scss'
+import PopUpSearch from './popUpSearch/PopUpSearch'
+import { useSearchInput } from './useSearchInput'
+
+interface Props {
+	setIsFinded: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const SearchInput: FC<Props> = ({ setIsFinded }) => {
+	const [value, setValue] = useState<string>('')
+	const { ref, isShow, setIsShow } = useOnClickOutside({
+		isInitialValue: false
+	})
+	const { users, err } = useSearchInput({ value, setIsShow })
+
+	useEffect(() => {
+		setIsFinded(isShow)
+	}, [isShow, setIsFinded])	
+
+	return (
+		<div className={s.container} ref={ref}>
+			<div className={s.inputSearch}>
+				<CiSearch className={s.searchIcon} />
+				<input
+					placeholder='Search'
+					value={value}
+					onChange={e => setValue(e.target.value)}
+				/>
+			</div>
+			{isShow && !err ? (
+				<div className={s.listCont}>
+					<ul>
+						{users.map(user => (
+							<>
+								{user.uid !== auth.currentUser?.uid && (
+									<li key={user.displayName}>
+										<PopUpSearch user={user} setValue={setValue} />
+									</li>
+								)}
+							</>
+						))}
+					</ul>
+				</div>
+			) : (
+				err && <span>Error</span>
+			)}
+		</div>
+	)
+}
+
+export default SearchInput
