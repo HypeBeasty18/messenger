50811ccec9c5311ee4396b643a226608d7c72cda
Dima <dimadeg2202@gmail.com>
2024-02-14 20:07:09 +0500

    Data of control's buttons messanger

---
+++ src/data/userControls.data.ts
@@ -0,0 +1,49 @@
+
+
+export const userControls = [
+	{
+		name: 'Dashboard',
+		icon: 'LuLayoutDashboard',
+		navigate: '/'
+	},
+	{
+		name: 'Chats',
+		icon: 'LuMessagesSquare',
+		navigate: '/'
+
+	},
+	{
+		name: 'Calls',
+		icon: 'LuPhone',
+		navigate: '/'
+
+	},
+	{
+		name: 'Users',
+		icon: 'LuUsers',
+		navigate: '/'
+
+	},
+	{
+		name: 'Notifications',
+		icon: 'MdOutlineNotificationsNone',
+		navigate: '/'
+
+	},
+	{
+		name: 'Videos',
+		icon: 'MdOutlineVideoLibrary',
+	},
+	{
+		name: 'Games',
+		icon: 'LuGamepad2',
+		navigate: '/'
+
+	},
+	{
+		name: 'Settings',
+		icon: 'LuSettings',
+		navigate: '/'
+
+	}
+]
