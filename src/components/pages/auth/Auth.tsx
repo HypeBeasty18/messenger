b434a2f3ef57b76a6c5a8f8b6b3d383b809a413b
Dima <dimadeg2202@gmail.com>
2024-02-14 20:05:31 +0500

    Added layout

--- src/components/pages/auth/Auth.tsx
+++ src/components/pages/auth/Auth.tsx
@@ -1,3 +1,4 @@
+import LayoutGuest from 'components/layoutGuest/LayoutGuest'
 import FormBtn from 'components/ui/formBtn/FormBtn'
 import InputData from 'components/ui/inputData/InputData'
 import { useAuth } from 'hooks/useAuth'
@@ -13,80 +14,74 @@ const Auth: FC = () => {
 		onSubmit,
 		errors,
 		error,
-		handleGoogle,
+		handleGoogle
 	} = useAuth()
 
 	return (
-		<div className={s.container}>
-			<div className={s.inner}>
-				<header>
-					<img src='/src/assets/icons/logo.png' draggable={false} />
-				</header>
+		<LayoutGuest>
+			<div className={s.container}>
+				<h3>Welcome!</h3>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className={s.inputsCont}>
+						<InputData
+							error={errors?.username?.message}
+							autoComplete='email'
+							name='username'
+							register={register}
+							options={{
+								required: 'Email is required',
+								pattern: {
+									value: /\S+@\S+\.\S+/,
+									message: 'Incorrect email format'
+								}
+							}}
+							type='text'
+							placeholder='Enter email'
+						/>
+						<InputData
+							error={errors?.password?.message}
+							name='password'
+							register={register}
+							options={{
+								required: 'Password is required',
+								minLength: {
+									value: 5,
+									message: 'Minimum 5 characters'
+								}
+							}}
+							type='password'
+							placeholder='Enter password'
+						/>
+					</div>
+					{error && <span className='errorData'>{error}</span>}
 
-				<main>
-					<h3>Welcome!</h3>
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<div className={s.inputsCont}>
-							<InputData
-								error={errors?.username?.message}
-								autoComplete='email'
-								name='username'
-								register={register}
-								options={{
-									required: 'Email is required',
-									pattern: {
-										value: /\S+@\S+\.\S+/,
-										message: 'Incorrect email format'
-									}
-								}}
-								type='text'
-								placeholder='Enter email'
-							/>
-							<InputData
-								error={errors?.password?.message}
-								name='password'
-								register={register}
-								options={{
-									required: 'Password is required',
-									minLength: {
-										value: 5,
-										message: 'Minimum 5 characters'
-									}
-								}}
-								type='password'
-								placeholder='Enter password'
-							/>
-						</div>
-						{error && <span className='errorData'>{error}</span>}
-
-						<div className={s.buttonCont}>
-							<FormBtn
-								text={'LogIn'}
-								type={'submit'}
-								fontSize={'18px'}
-								pInline={'20px'}
-								padding={'10px'}
-								width={'100%'}
-								onClick={() => setType('login')}
-							/>
-							<FormBtn
-								text={'SignUp'}
-								type={'submit'}
-								fontSize={'18px'}
-								pInline={'20px'}
-								padding={'10px'}
-								width={'100%'}
-								onClick={() => setType('signup')}
-							/>
-						</div>
-					</form>
-					<span className={s.divide}>or</span>
-					<button className={s.googleButton} onClick={handleGoogle}>
-						<span>Log in with Google</span>
-					</button>
-				</main>
+					<div className={s.buttonCont}>
+						<FormBtn
+							text={'LogIn'}
+							type={'submit'}
+							fontSize={'18px'}
+							pInline={'20px'}
+							padding={'10px'}
+							width={'100%'}
+							onClick={() => setType('login')}
+						/>
+						<FormBtn
+							text={'SignUp'}
+							type={'submit'}
+							fontSize={'18px'}
+							pInline={'20px'}
+							padding={'10px'}
+							width={'100%'}
+							onClick={() => setType('signup')}
+						/>
+					</div>
+				</form>
+				<span className={s.divide}>or</span>
+				<button className={s.googleButton} onClick={handleGoogle}>
+					<span>Log in with Google</span>
+				</button>
 			</div>
-		</div>
+		</LayoutGuest>
 	)
 }
 
